e668c44f89bb593acf3f2aa1665e27d07b29974f
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-03 17:55:44 -0500

    Add signup form

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 const loginForm = document.getElementById("loginForm");
+const signupForm = document.getElementById("signupForm");
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 
@@ -10,6 +11,30 @@ if (loginForm) {
       password: passwordInput.value,
     };
     const res = await fetch("/api/user/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (res.ok) {
+      location.href = "/";
+    } else {
+      const error = await res.json();
+      console.log(error);
+      alert("todo");
+    }
+  });
+}
+
+if (signupForm) {
+  signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const data = {
+      username: usernameInput.value,
+      password: passwordInput.value,
+    };
+    const res = await fetch("/api/user/signup", {
       method: "POST",
       headers: {
         "content-type": "application/json",
