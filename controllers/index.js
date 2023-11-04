455d56ddbb7006502e40de52e8f1a5845c1338df
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-03 20:30:55 -0500

    Apply substring to post content

--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,39 @@
 import { Router } from "express";
 import populateLocals from "../lib/middleware/populateLocals.js";
 import { Post, User } from "../lib/models/index.js";
+import sequelize from "../config/connection.js";
 
 const router = Router();
 
 router.use(populateLocals);
 
 router.get("/", async (req, res) => {
-  const posts = await Post.findAll({
-    order: [["createdAt", "DESC"]],
-    include: [
-      {
-        model: User,
-        as: "author",
-      },
-    ],
-    limit: 5,
-    raw: true,
-    nest: true,
-  });
+  const posts = (
+    await Post.findAll({
+      order: [["createdAt", "DESC"]],
+      include: [
+        {
+          model: User,
+          as: "author",
+        },
+      ],
+      attributes: [
+        "title",
+        "createdAt",
+        [
+          sequelize.fn("SUBSTRING", sequelize.col("content"), 1, 256),
+          "content",
+        ],
+        [sequelize.fn("LENGTH", sequelize.col("content")), "content_length"],
+      ],
+      limit: 5,
+      raw: true,
+      nest: true,
+    })
+  ).map((p) => ({
+    ...p,
+    content: p.content + (p.content.length < p.content_length ? "..." : ""),
+  }));
   console.log(posts);
   res.render("index", { pageSubtitle: "Home", posts });
 });
