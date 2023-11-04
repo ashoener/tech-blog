815956889fc1b175ff8c533041e839ac5418cb7f
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-03 21:47:08 -0500

    Add update post api route

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -6,7 +6,9 @@ import requireLoggedIn from "../../lib/middleware/requireLoggedIn.js";
 
 const router = Router();
 
-router.post("/", requireLoggedIn, async (req, res) => {
+router.use(requireLoggedIn);
+
+router.post("/", async (req, res) => {
   try {
     const newPost = Post.build({
       title: req.body.title,
@@ -16,6 +18,23 @@ router.post("/", requireLoggedIn, async (req, res) => {
     await newPost.validate();
     await newPost.save({ validate: false });
     res.json({ success: true, link: `/posts/${newPost.id}` });
+  } catch (err) {
+    handleError(err, res);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findByPk(req.params.id);
+    if (!post)
+      return res
+        .status(404)
+        .json({ success: false, errors: ["Post not found"] });
+    post.title = req.body.title;
+    post.content = req.body.content;
+    await post.validate();
+    await post.save({ validate: false });
+    res.json({ success: true, link: `/posts/${post.id}` });
   } catch (err) {
     handleError(err, res);
   }
