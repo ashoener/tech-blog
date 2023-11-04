f51d69c98940b42bdabf42297551d92f79550eff
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-03 20:15:32 -0500

    Render posts on home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import populateLocals from "../lib/middleware/populateLocals.js";
-import { Post } from "../lib/models/index.js";
+import { Post, User } from "../lib/models/index.js";
 
 const router = Router();
 
@@ -9,9 +9,17 @@ router.use(populateLocals);
 router.get("/", async (req, res) => {
   const posts = await Post.findAll({
     order: [["createdAt", "DESC"]],
+    include: [
+      {
+        model: User,
+        as: "author",
+      },
+    ],
     limit: 5,
     raw: true,
+    nest: true,
   });
+  console.log(posts);
   res.render("index", { pageSubtitle: "Home", posts });
 });
 
