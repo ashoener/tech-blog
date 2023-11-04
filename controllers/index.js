101b62861200c3c1a65ffc89c2b8ff7d7f06ed11
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-03 19:52:12 -0500

    Send posts to home route

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import populateLocals from "../lib/middleware/populateLocals.js";
+import { Post } from "../lib/models/index.js";
 
 const router = Router();
 
 router.use(populateLocals);
 
-router.get("/", (req, res) => {
-  res.render("index", { pageSubtitle: "Home" });
+router.get("/", async (req, res) => {
+  const posts = await Post.findAll({
+    order: [["createdAt", "DESC"]],
+    limit: 5,
+    raw: true,
+  });
+  res.render("index", { pageSubtitle: "Home", posts });
 });
 
 export default router;
